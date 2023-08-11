e7023cd01afc8dee1f6c644c544e05e5f8147741
FoggyFinder <FoggyFinder@yandex.com>
2023-08-11 12:43:26 +0300

    remove realm

--- NativeAotSample1/RealmLibrary/Class1.cs
+++ NativeAotSample1/RealmLibrary/Class1.cs
@@ -1,60 +1,33 @@
 ﻿using ClassLibrary1;
-using Realms;
 
 namespace RealmLibrary
 {
-    public static class RealmConfig
+    internal partial class RFoo
     {
-        private readonly static RealmConfiguration AppConfig =
-            new("t1.realm")
-            {
-                IsReadOnly = true,
-                Schema = new[]
-                    {
-                        typeof(RFoo),
-                        typeof(RBar)
-                    },
-            };
-
-        public static Realm GetAppReadOnlyRealm() => Realm.GetInstance(AppConfig);
-    }
-
-    internal partial class RFoo : IRealmObject
-    {
-        [PrimaryKey]
         public string Key { get; set; } = null!;
         public string Value { get; set; } = null!;
-        public static RFoo FromFoo(Foo f, Realm realm)
+        public static RFoo FromFoo(Foo f)
         {
-            var rf = realm.Find<RFoo>(f.Key);
-            if (rf != null) { return rf; }
-            else
+            return new RFoo
             {
-                rf = new RFoo
-                {
-                    Key = f.Key,
-                    Value = f.Value
-                };
-                realm.Add(rf);
-                return rf;
-            }
+                Key = f.Key,
+                Value = f.Value
+            };
         }
     }
 
-    internal partial class RBar : IRealmObject
+    internal partial class RBar
     {
-        [PrimaryKey]
         public Guid Guid { get; set; }
         public IList<RFoo> Xs { get; } = null!;
 
-        public static RBar FromCourse(Bar b, Realm realm)
+        public static RBar FromCourse(Bar b)
         {
             var r_c = new RBar { Guid = b.Guid };
             foreach (Foo f in b.Xs)
             {
-                r_c.Xs.Add(RFoo.FromFoo(f, realm));
+                r_c.Xs.Add(RFoo.FromFoo(f));
             }
-            realm.Add(r_c);
             return r_c;
         }
     }
